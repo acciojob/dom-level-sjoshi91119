418378e5d1c626d09d353f4c6684082340be68d5
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-20 03:34:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let lev=document.getElementById("level").value;
+
+alert("The level of the element is: "+lev);
+// const element = document.getElementById("level");
+// alert(`The level of the element is: ${element}`);
